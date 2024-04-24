c720cd46d1ae9093e7400bfda353d6f061b5eca2
amrutha <amruthatc86@gmail.com>
2024-04-24 13:24:39 +0400

    first commit

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Todo from './Components/Todo'
+import TodoItems from './Components/TodoItems'
+
+
+
+
+
+const App=()=> {
+  return (
+    <div>
+    <Todo/>
+    <TodoItems/>
+ </div>
+)
+}
+
+export default App
